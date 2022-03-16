dd6376b3f4ba8aada699f8457fd3c4f64e8b14d1
Catherine Deas <catherine.deas@madetech.com>
2022-03-16 22:19:40 +0000

    Refactored roman.rb

--- lib/roman.rb
+++ lib/roman.rb
@@ -1,13 +1,7 @@
 class Roman
 
     def convert_to_number(string)
-        if string == ""
-            return 0
-        elsif string == "I"
-            return 1
-        elsif string == "II"
-            return 2
-        end
+        return string.size
     end
     
 end
